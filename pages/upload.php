55d169ed1b6ffd13582df2997bb82163ead1133e
yslamgulypirgulyyev <islamguly28@gmail.com>
2022-03-05 12:16:57 +0100

    Style the form

--- pages/upload.php
+++ pages/upload.php
@@ -2,32 +2,34 @@
     Name,Author,ISBN,ReleaseDate,Price,desciption
 -->
 
-<form action="" method="post">
-    <div class="">
-        <label for="name">Name</label>
-        <input type="text" name="name">
-    </div>   
-    <div>
-        <label for="author">Author</label>
-        <input type="text" name="author">
-    </div>
-    <div>
-        <label for="ISBN">ISBN</label>
-        <input type="text" name="ISBN">
-    </div>
-    <div>
-        <label for="releaseDate">Release Date</label>
-        <input type="date" name="releaseDate"> <!--ask-->
-    </div>    
-    <div>
-        <label for="price">Price</label>
-        <input type="number" name="price">
-    </div>
-    <div>
-        <label for="desciption">Desciption</label>
-        <textarea name="description"></textarea>
-    </div>
-    <div>
-        <button type="submit">Upload</button>
-    </div>
-</form>
+<div class="page page-upload">
+    <form method="post" class="card">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" name="name">
+        </div>   
+        <div class="form-group">
+            <label for="author">Author</label>
+            <input type="text" name="author">
+        </div>
+        <div class="form-group">
+            <label for="ISBN">ISBN</label>
+            <input type="text" name="ISBN">
+        </div>
+        <div class="form-group">
+            <label for="releaseDate">Release Date</label>
+            <input type="date" name="releaseDate"> <!--ask-->
+        </div>    
+        <div class="form-group">
+            <label for="price">Price</label>
+            <input type="number" name="price">
+        </div>
+        <div class="form-group">
+            <label for="desciption">Desciption</label>
+            <textarea name="description"></textarea>
+        </div>
+        <div>
+            <button type="submit">Upload</button>
+        </div>
+    </form> 
+</div>
--- views/_header.php
+++ views/_header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo site</title>
-    <link rel="stylesheet" href="/test/css/app.css">
+    <link rel="stylesheet" href="/Test/css/app.css">
 </head>
 <body>
     <header>
