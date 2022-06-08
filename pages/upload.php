9ca2c310eae64e8b0e7cbd3c5845914b77678cd8
yslamgulypirgulyyev <islamguly28@gmail.com>
2022-06-08 23:13:09 +0200

    specify by which user was the book added

--- pages/upload.php
+++ pages/upload.php
@@ -1,11 +1,12 @@
 <?php 
 if(!defined("APP_VERSION")){
     exit;
-}?>
+}
+gate();
+?>
 <?php  
 
 $errors=[];
-
     if (is_post()){
        
         $name = trim($_POST['name']);
@@ -20,18 +21,15 @@ $errors=[];
         if(count($errors)==0){
             // save values in the database
             // success message
-
-            //todo to change this later
-            //$image='fe';
-            $sql = $db->prepare("INSERT INTO books (`name`,`author`,`isbn`,`release_date`,`price`,`description`) VALUES (?, ?, ?, ?, ?, ?)");
-            $sql->bind_param('ssssis',$name,$author,$isbn,$releaseDate,$price,$description);
+            $user_id = current_user_id();
+            $sql = $db->prepare("INSERT INTO books (`name`,`author`,`isbn`,`release_date`,`price`,`description`,`user_id`) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $sql->bind_param('ssssisi',$name,$author,$isbn,$releaseDate,$price,$description,$user_id);
             $sql->execute();
             //die_dump($sql->sqlstate);
             //die_dump($sql);
             $sql->close();
             
             $new_id = $db -> insert_id;
-
             redirect('edit',['id' => $new_id]);
         }
     } 
