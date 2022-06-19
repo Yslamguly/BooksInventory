3abdded5fdcbd5522ac42c00a4f7b87e26ee19c6
yslamgulypirgulyyev <islamguly28@gmail.com>
2022-06-19 23:10:52 +0200

    styled the page

--- pages/about.php
+++ pages/about.php
@@ -3,16 +3,16 @@ if(!defined("APP_VERSION")){
     exit;
 }?>
 <?php require_once "./views/_header.php";?>
-<div class="container"><img class="cover" src="images/books-1673578_1280.png"></div>
-    <h1> 
-         About 
-    </h1>
-    <div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae neque odio voluptatum ipsa fugiat dolor magni iste vel quas eligendi! Accusamus sapiente error minima consequuntur quidem tenetur voluptates. Commodi, cupiditate!</p>
-        <p>Delectus minus explicabo dolor fugit ut! Optio nesciunt incidunt sit quo quasi error necessitatibus et ut. Nam dolorem hic repudiandae maxime numquam, animi deleniti, veniam nobis libero, fugiat neque inventore?</p>
-        <p>Reiciendis asperiores quis at doloremque cum. Maiores rerum veniam natus illo tempora quo culpa molestias rem, enim incidunt dignissimos, non itaque? Magni ullam hic placeat officia voluptatem, in odio maiores.</p>
-        <p>Iste doloribus, fugit praesentium eligendi mollitia adipisci, iure vero corporis obcaecati amet cumque et. Dignissimos accusantium, tempore culpa rem veniam totam exercitationem repudiandae tempora, optio laborum, nulla dolore beatae numquam.</p>
-        <p>Delectus minima alias, iure eveniet voluptas sunt voluptatum ullam cupiditate rem enim, incidunt quibusdam? Dolores ab ex dolore rerum qui amet maxime officia nam modi, perspiciatis laboriosam inventore. Sit, hic?</p>
+<div class="about-section">
+    <div class="inner-container">
+        <h1>About Us</h1>
+        <p class="text">
+            We are one of the largest web-sites devoted for book. We are delighted to provide services for you in order to make people discover the best and make this world better.
+        </p>
+        <h1>Contact Us</h1>
+        <p class="text">Email: <a href="mailto:libri-support@example.com">libri-support@example.com</a></p>
+        <p class="text">Phone Number: <a href="tel:+36204888414">+36 20 488 8414</a></p>
+    </div>
 </div>
 
 <?php require_once "./views/_footer.php"; ?>
